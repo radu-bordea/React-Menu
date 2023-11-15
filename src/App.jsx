bf9f0513c0bfd825ee7eb79f5fef58745c72ef34
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-15 13:30:46 +0100

    create menu, menuitem title components and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import menu from "./data";
+import Title from "./Title";
+import Menu from "./Menu";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
